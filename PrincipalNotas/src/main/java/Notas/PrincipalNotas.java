1f8acf99f94b6a2c12a60d54821f07675b821a21
Mateog777 <141966605+Mateog777@users.noreply.github.com>
2024-09-10 12:10:27 -0500

    Add files via upload

---
+++ PrincipalNotas/src/main/java/Notas/PrincipalNotas.java
@@ -0,0 +1,12 @@
+package Notas;
+
+public class PrincipalNotas {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setResizable(false);
+        ventana.setTitle("Notas");
+        ventana.show(true);
+
+    }
+}
